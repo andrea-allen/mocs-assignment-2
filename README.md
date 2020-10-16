06e52d29d441b3e541c3b564730e8427ee53c212
Andrea Allen <andrea2allen@gmail.com>
2020-10-16 09:44:50 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mocs-assignment-2
 MOCS Assignment 2
+
+Andrea Allen and Amanda Bertschinger
---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    print('MOCS Assignment 2, 10-26-2020')
